7a906e1de144cda58c72445a73cedd5b7bdb16cd
Lucas <lucasparza@gmail.com>
2018-09-18 21:03:18 -0300

    trashed items

--- application/network.sh
+++
@@ -1,5 +0,0 @@
-docker pull hyperledger/fabric-peer:latest
-docker pull hyperledger/fabric-orderer:latest
-docker pull hyperledger/fabric-ca:latest
-docker pull hyperledger/fabric-ccenv:latest
-docker-compose -f test/fixtures/docker-compose-2orgs-4peers-tls.yaml up
---
+++ application/trash/network.sh
@@ -0,0 +1,1 @@
+docker-compose -f test/fixtures/docker-compose-2orgs-4peers-tls.yaml up
