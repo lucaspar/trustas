89d4ef32b7ec2f6873f7846e1e7b72bd77a159b1
Lucas <lucasparza@gmail.com>
2018-09-29 16:48:15 -0300

    included sample of transaction and block query

---
+++ src/network/samples/block_sample.py
@@ -0,0 +1,243 @@
+BLOCK_SAMPLE = {
+  'data': {
+    'data': [
+      {
+        'payload': {
+          'data': {
+            'actions': [
+              {
+                'header': {
+                  'creator': {
+                    'id_bytes': '-----BEGIN CERTIFICATE-----\nMIICGTCCAb+gAwIBAgIQJaHcEQIxGGfXWqBy7rectzAKBggqhkjOPQQDAjBzMQsw\nC
+                                 QYDVQQGEwJVUzETMBEGA1UECBMKQ2FsaWZvcm5pYTEWMBQGA1UEBxMNU2FuIEZy\nYW5jaXNjbzEZMBcGA1UEChMQb3JnMS5
+                                 leGFtcGxlLmNvbTEcMBoGA1UEAxMTY2Eu\nb3JnMS5leGFtcGxlLmNvbTAeFw0xNzA2MjMwOTQ1MThaFw0yNzA2MjEwOTQ1M
+                                 Tha\nMFsxCzAJBgNVBAYTAlVTMRMwEQYDVQQIEwpDYWxpZm9ybmlhMRYwFAYDVQQHEw1T\nYW4gRnJhbmNpc2NvMR8wHQYDV
+                                 QQDDBZBZG1pbkBvcmcxLmV4YW1wbGUuY29tMFkw\nEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEu4qVQNMjzforwk3BMRz9Kp7
+                                 ro4h5eorT\nTbcUkRoZvrVo2TLirMgUwjNtJW92Dgv55QD8yA82+qK1cZkQ0rEUE6NNMEswDgYD\nVR0PAQH/BAQDAgeAMAw
+                                 GA1UdEwEB/wQCMAAwKwYDVR0jBCQwIoAg8vWgjVlptKIf\nmbVmekJlZKPmQKJo0va/53oOBm77Qm8wCgYIKoZIzj0EAwIDS
+                                 AAwRQIhAJ4RLI6T\nKNdbZuP2kUY4RAvGjGhKTJaXpt7X1ANZES8UAiBg6qwCdoS2NBfj5rwEp+WXcjSE\npglP5YqMzyFRx
+                                 6iOjA==\n-----END CERTIFICATE-----\n'
+                    'mspid': 'Org1MSP',
+                  },
+                  'nonce': 'a9eeb590924d3df8b132106f017a0a61c0bbb5f355e76113'
+                },
+                'payload': {
+                  'action': {
+                    'endorsements': [
+                      {
+                        'endorser': {
+                          'id_bytes': '-----BEGIN -----\nMIICGjCCAcCgAwIBAgIRANh4WMem0yRYnWgLkJlei4gwCgYIKoZIzj0EAwIwczEL\nMAkGA1
+                                       UEBhMCVVMxEzARBgNVBAgTCkNhbGlmb3JuaWExFjAUBgNVBAcTDVNhbiBG\ncmFuY2lzY28xGTAXBgNVBAoTEG9yZz
+                                       EuZXhhbXBsZS5jb20xHDAaBgNVBAMTE2Nh\nLm9yZzEuZXhhbXBsZS5jb20wHhcNMTcwNjIzMDk0NTE4WhcNMjcwNj
+                                       IxMDk0NTE4\nWjBbMQswCQYDVQQGEwJVUzETMBEGA1UECBMKQ2FsaWZvcm5pYTEWMBQGA1UEBxMN\nU2FuIEZyYW5j
+                                       aXNjbzEfMB0GA1UEAxMWcGVlcjAub3JnMS5leGFtcGxlLmNvbTBZ\nMBMGByqGSM49AgEGCCqGSM49AwEHA0IABAFv
+                                       P6xBxkAs9WWzb0+Ynq36fjfesRnb\n+hJj7rwA2I0Uwi8zHwshHNx3G9/oIkggq3RkxU9aTr9mQl7kGavkpOqjTTBL
+                                       MA4G\nA1UdDwEB/wQEAwIHgDAMBgNVHRMBAf8EAjAAMCsGA1UdIwQkMCKAIPL1oI1ZabSi\nH5m1ZnpCZWSj5kCiaN
+                                       L2v+d6DgZu+0JvMAoGCCqGSM49BAMCA0gAMEUCIQDKNqFx\njCouRdMVjsrl/QtncRHkJ6xlAF3Nyl+QilMnUwIgeW
+                                       Bk5TI6768o2I/cGLiFy0EI\nxEsqFNCLbh4Bfarqkqw=\n-----END -----\n'
+                          'mspid': 'Org1MSP',
+                        },
+                        'signature': '0D\x02 n\x81w\x8b\xa6\xf2fd\x8d~\xeci\xd4"\x9dl\x05\x18\n\xf3\xb0\x98\x80\xa7\x86\xec\x92\x
+                                      d0\x7fy~\xc0\x02 D\x7f\xf4\x99\\\xb8\xaf7\xde\x08\xe1i\x9a*j\xc87\xcd\xd1
+                                      [\x98\x08y\x801]\xb7\xd7Z\x1cZ'
+                      },
+                      {
+                        'endorser': {
+                          'id_bytes': '-----BEGIN -----\nMIICGTCCAb+gAwIBAgIQfv6qSshCTcQbqKE1ciGNfTAKBggqhkjOPQQDAjBzMQsw\nCQYDVQ
+                                       QGEwJVUzETMBEGA1UECBMKQ2FsaWZvcm5pYTEWMBQGA1UEBxMNU2FuIEZy\nYW5jaXNjbzEZMBcGA1UEChMQb3JnMS
+                                       5leGFtcGxlLmNvbTEcMBoGA1UEAxMTY2Eu\nb3JnMS5leGFtcGxlLmNvbTAeFw0xNzA2MjMwOTQ1MThaFw0yNzA2Mj
+                                       EwOTQ1MTha\nMFsxCzAJBgNVBAYTAlVTMRMwEQYDVQQIEwpDYWxpZm9ybmlhMRYwFAYDVQQHEw1T\nYW4gRnJhbmNp
+                                       c2NvMR8wHQYDVQQDExZwZWVyMS5vcmcxLmV4YW1wbGUuY29tMFkw\nEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEqvzr
+                                       yjXz3HKDr5EU+B5tYMowe+ygpAwZ\n4mSZZM2YiObIyz1zEO2Z+8wzH/J17NUQrzarnj1P3cLix5dPg3LtAaNNMEsw
+                                       DgYD\nVR0PAQH/BAQDAgeAMAwGA1UdEwEB/wQCMAAwKwYDVR0jBCQwIoAg8vWgjVlptKIf\nmbVmekJlZKPmQKJo0v
+                                       a/53oOBm77Qm8wCgYIKoZIzj0EAwIDSAAwRQIhANvKYs4n\numGFD5IwXsjAxDGwEQBefESEUNKbMPyW4VR/AiB/lN
+                                       D/Qbp7W0S6ICc5N1tgDOeV\nHnVlRvTUGkujWFQ5Qw==\n-----END -----\n'
+                          'mspid': 'Org1MSP',
+                        },
+                        'signature': '0D\x02 alb\xe1\xd9:\xb2\x82\x9d\xf3\xb5Q\x12<\xc3\xb3vm\xac\\C\xca\x19\x98\x8f\xa6\x18s\xf1
+                                      \x895\x02 %/\x8e. j\xc7\xec\x9a\x96Q\x11Y\x1c\xbf\x9ab\xc1\xebmeV\xe0%3\x0f\xf5\xa0@\x1ezm'
+                      },
+                    ],
+                    'proposal_response_payload': {
+                      'extension': {
+                        'chaincode_id': {
+                          'name': 'lscc',
+                          'path': '',
+                          'version': '1.0.0',
+                        },
+                        'events': {
+                          'chaincode_id': '',
+                          'event_name': '',
+                          'payload': ''
+                          'tx_id': '',
+                        },
+                        'response': {
+                          'message': '',
+                          'payload': '\n\nexample_cc\x12\x031.0\x1a\x04escc"\x04vscc*(\x12\x0c\x12\n\x08\x01\x12\x02\x08\x00\x12\
+                                      x02\x08\x01\x1a\x0b\x12\t\n\x07Org1MSP\x1a\x0b\x12\t\n\x07Org2MSP2D\n \x14\xa0\x9a\x84q\xd7
+                                      |\x16!(Yi\xa2\x18\x9dLx\xd5\x9f\x86\xa3\xcd\xcbm\xe8\x1d\x16\x03\xe4\xb8\xb0$\x12 \x9eD\xb7
+                                      2\xc2\xed\xef\xe1\xf3\xa5\xe1\x96\xcf\xeb~\xb2\xf9\xda\xda{\x0f\x87\x97\x12i"\xd3\xd4\x08SL
+                                      #: U\xafsvuF\xe0\xb4}~\xb8{\xdfGP\x17^\xab\xd3\x1b\x80\xd6\x0fP1\x9d\xe1\x8e3\x1f\x15\xccB,
+                                      \x12\x0c\x12\n\x08\x01\x12\x02\x08\x00\x12\x02\x08\x01\x1a\r\x12\x0b\n\x07Org1MSP\x10\x01\x
+                                      1a\r\x12\x0b\n\x07Org2MSP\x10\x01'
+                          'status': 200,
+                        },
+                        'results': {
+                          'data_model': 0,
+                          'ns_rwset': [
+                            {
+                              'namespace': 'example_cc',
+                              'rwset': {
+                                'range_queries_info': [],
+                                'reads': [],
+                                'writes': [
+                                  {
+                                    'is_delete': False,
+                                    'key': 'a',
+                                    'value': '200'
+                                  },
+                                  {
+                                    'is_delete': False,
+                                    'key': 'b',
+                                    'value': '300'
+                                  },
+                                ],
+                              },
+                            },
+                            {
+                              'namespace': 'lscc',
+                              'rwset': {
+                                'range_queries_info': [],
+                                'reads': [
+                                  {
+                                    'key': 'example_cc',
+                                    'version': {
+                                      'block_num': '0',
+                                      'tx_num': '0',
+                                    },
+                                  },
+                                ],
+                                'writes': [
+                                  {
+                                    'is_delete': False,
+                                    'key': 'example_cc',
+                                    'value': '\n\nexample_cc\x12\x031.0\x1a\x04escc"\x04vscc*(\x12\x0c\x12\n\x08\x01\x12\x02\x08\
+                                              x00\x12\x02\x08\x01\x1a\x0b\x12\t\n\x07Org1MSP\x1a\x0b\x12\t\n\x07Org2MSP2D\n \x14\
+                                              xa0\x9a\x84q\xd7|\x16!(Yi\xa2\x18\x9dLx\xd5\x9f\x86\xa3\xcd\xcbm\xe8\x1d\x16\x03\xe
+                                              4\xb8\xb0$\x12 \x9eD\xb72\xc2\xed\xef\xe1\xf3\xa5\xe1\x96\xcf\xeb~\xb2\xf9\xda\xda{
+                                              \x0f\x87\x97\x12i"\xd3\xd4\x08SL#: U\xafsvuF\xe0\xb4}~\xb8{\xdfGP\x17^\xab\xd3\x1b\
+                                              x80\xd6\x0fP1\x9d\xe1\x8e3\x1f\x15\xccB,\x12\x0c\x12\n\x08\x01\x12\x02\x08\x00\x12\
+                                              x02\x08\x01\x1a\r\x12\x0b\n\x07Org1MSP\x10\x01\x1a\r\x12\x0b\n\x07Org2MSP\x10\x01'
+                                  },
+                                ],
+                              },
+                            },
+                          ],
+                        },
+                      },
+                      'proposal_hash': '022ec4c2e4919dab4131eef00c1f72c340e44952954c5f28d5b0208140d00a52'
+                    },
+                  },
+                  'chaincode_proposal_payload': {
+                    'input': '\nr\x08\x01\x12\x06\x12\x04lscc\x1af\n\x06deploy\n\x0fbusinesschannel\n=\n-\x08\x01\x12\x11\x12\nex
+                              ample_cc\x1a\x031.0\x1a\x16\n\x04init\n\x01a\n\x03200\n\x01b\n\x03300\x12\x0c\x08\xc6\x80\xbf\xdd\x
+                              05\x10\xf0\x92\xf5\xe4\x01\n\x00\n\x04escc\n\x04vscc'
+                  },
+                },
+              },
+            ],
+          },
+          'header': {
+            'channel_header': {
+              'channel_id': 'businesschannel',
+              'epoch': 0,
+              'extension': '\x12\x06\x12\x04lscc'
+              'timestamp': '2018-09-29 18:11:40',
+              'tx_id': 'd7fa9d4134b865336c94b155ee604b0777d56bc04d6b263e16faed0a59b0adb8',
+              'type': 3,
+              'type_string': 'ENDORSER_TRANSACTION',
+              'version': 1,
+            },
+            'signature_header': {
+              'creator': {
+                'id_bytes': '-----BEGIN CERTIFICATE-----\nMIICGTCCAb+gAwIBAgIQJaHcEQIxGGfXWqBy7rectzAKBggqhkjOPQQDAjBzMQsw\nCQYDV
+                             QQGEwJVUzETMBEGA1UECBMKQ2FsaWZvcm5pYTEWMBQGA1UEBxMNU2FuIEZy\nYW5jaXNjbzEZMBcGA1UEChMQb3JnMS5leGFtcGx
+                             lLmNvbTEcMBoGA1UEAxMTY2Eu\nb3JnMS5leGFtcGxlLmNvbTAeFw0xNzA2MjMwOTQ1MThaFw0yNzA2MjEwOTQ1MTha\nMFsxCzA
+                             JBgNVBAYTAlVTMRMwEQYDVQQIEwpDYWxpZm9ybmlhMRYwFAYDVQQHEw1T\nYW4gRnJhbmNpc2NvMR8wHQYDVQQDDBZBZG1pbkBvc
+                             mcxLmV4YW1wbGUuY29tMFkw\nEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEu4qVQNMjzforwk3BMRz9Kp7ro4h5eorT\nTbcUkRoZv
+                             rVo2TLirMgUwjNtJW92Dgv55QD8yA82+qK1cZkQ0rEUE6NNMEswDgYD\nVR0PAQH/BAQDAgeAMAwGA1UdEwEB/wQCMAAwKwYDVR0
+                             jBCQwIoAg8vWgjVlptKIf\nmbVmekJlZKPmQKJo0va/53oOBm77Qm8wCgYIKoZIzj0EAwIDSAAwRQIhAJ4RLI6T\nKNdbZuP2kUY
+                             4RAvGjGhKTJaXpt7X1ANZES8UAiBg6qwCdoS2NBfj5rwEp+WXcjSE\npglP5YqMzyFRx6iOjA==\n-----END
+                             CERTIFICATE-----\n'
+                'mspid': 'Org1MSP',
+              },
+              'nonce': 'a9eeb590924d3df8b132106f017a0a61c0bbb5f355e76113'
+            },
+          },
+        },
+        'signature': '0D\x02 d\xfdp;E\xbc\xb3\x9c7\x06\xc1a\xb2\\\xc9ga\xc9swel\xa1\xbc\xc4\xa7\x05\x1a\xcf\xb9\n\xe2\x02
+                      &5%\xce+o\xdb;\xea\x1d|c&\xb1\xb4\xad\xc9\x0e\xff\xa3k]^\xf6"\x18\x06\xb1\xb3E\xb0}'
+      },
+    ],
+  },
+  'header': {
+    'data_hash': 'e39bce6605c4bd5a4ec6838aef466b7db430ed1480713d60f942b7c328275c80'
+    'number': 1,
+    'previous_hash': 'e9ca664c7aca29bb157c64922e090f8e6b2665504e87a217bc53fea30a65861d'
+  },
+  'metadata': {
+    'metadata': [
+      {
+        'signatures': [
+          {
+            'signature': '0D\x02 m\xf3;\x13\xa5\xf3\xcd\xab\xde*ZY\xcf\xff\x08\x7fvu.\xb8\x8b"\xb2\x03t|NRJY<\xac\x02
+                          k\x89j\xdf\xf61\xf4\x92\xc5^;\x15S\xe2Q)\xc1O\x8c\xc2\xa8\xf1\x1f\xd7\xea\x05\x8d\t_&\xec\x84'
+            'signature_header': {
+              'creator': {
+                'id_bytes': '-----BEGIN -----\nMIICCzCCAbKgAwIBAgIQAxuKWC5I9BHkcXlnlgFGdDAKBggqhkjOPQQDAjBpMQsw\nCQYDVQQGEwJVUzET
+                             MBEGA1UECBMKQ2FsaWZvcm5pYTEWMBQGA1UEBxMNU2FuIEZy\nYW5jaXNjbzEUMBIGA1UEChMLZXhhbXBsZS5jb20xFzAVBgNVBA
+                             MTDmNhLmV4YW1w\nbGUuY29tMB4XDTE3MDYyMzA5NDUxOFoXDTI3MDYyMTA5NDUxOFowWDELMAkGA1UE\nBhMCVVMxEzARBgNVBA
+                             gTCkNhbGlmb3JuaWExFjAUBgNVBAcTDVNhbiBGcmFuY2lz\nY28xHDAaBgNVBAMTE29yZGVyZXIuZXhhbXBsZS5jb20wWTATBgcq
+                             hkjOPQIBBggq\nhkjOPQMBBwNCAAQAIJj7b/PJbaGbd4lz3rxIQ+MA2cgxgbnYvmicFXLRffXjkNnI\nCSqpf2W4KvtGUKkXEYpe
+                             9aY3qqbN1tHycmDBo00wSzAOBgNVHQ8BAf8EBAMCB4Aw\nDAYDVR0TAQH/BAIwADArBgNVHSMEJDAigCCPyAkk5V5ghFPyzP5bzG
+                             PO6OmswsXK\nb1f6K6sRGkTqlDAKBggqhkjOPQQDAgNHADBEAiAI5eVt3H50U2l74alaGn8GOEHl\nLedZaegdRoHNJNCKGwIgGU
+                             E39OhXw6loZsRzvyCnWn7mWPF7jnV21MRKj/Kack4=\n-----END -----\n'
+                'mspid': 'OrdererMSP',
+              },
+              'nonce': '816a14e09be057f75b913aa6fb7ff5c5fb3f1a21f876df63'
+            },
+          },
+        ],
+        'value': ''
+      },
+      {
+        'signatures': [
+          {
+            'signature': '0D\x02
+                          l\x8d\xfd\xaelh\xa9e\x9b\x08t\x9c\xbf\x05\x16g\x85\xd0\xaa\xe5n\xc8%\x0e(i\x89\xca\xa3\x82\x13Y\x02 \x1
+                          0\x1e\xc8\xfd\nkv\x10\x18\\\x12\xa2\xec\xbb\xdf\xf8\xd7z\xe4\xe89\xc4\x85\xb3>\xef\x13\xf1\xc0\xd9\xae\
+                          xb1'
+            'signature_header': {
+              'creator': {
+                'id_bytes': '-----BEGIN -----\nMIICCzCCAbKgAwIBAgIQAxuKWC5I9BHkcXlnlgFGdDAKBggqhkjOPQQDAjBpMQsw\nCQYDVQQGEwJVUzET
+                             MBEGA1UECBMKQ2FsaWZvcm5pYTEWMBQGA1UEBxMNU2FuIEZy\nYW5jaXNjbzEUMBIGA1UEChMLZXhhbXBsZS5jb20xFzAVBgNVBA
+                             MTDmNhLmV4YW1w\nbGUuY29tMB4XDTE3MDYyMzA5NDUxOFoXDTI3MDYyMTA5NDUxOFowWDELMAkGA1UE\nBhMCVVMxEzARBgNVBA
+                             gTCkNhbGlmb3JuaWExFjAUBgNVBAcTDVNhbiBGcmFuY2lz\nY28xHDAaBgNVBAMTE29yZGVyZXIuZXhhbXBsZS5jb20wWTATBgcq
+                             hkjOPQIBBggq\nhkjOPQMBBwNCAAQAIJj7b/PJbaGbd4lz3rxIQ+MA2cgxgbnYvmicFXLRffXjkNnI\nCSqpf2W4KvtGUKkXEYpe
+                             9aY3qqbN1tHycmDBo00wSzAOBgNVHQ8BAf8EBAMCB4Aw\nDAYDVR0TAQH/BAIwADArBgNVHSMEJDAigCCPyAkk5V5ghFPyzP5bzG
+                             PO6OmswsXK\nb1f6K6sRGkTqlDAKBggqhkjOPQQDAgNHADBEAiAI5eVt3H50U2l74alaGn8GOEHl\nLedZaegdRoHNJNCKGwIgGU
+                             E39OhXw6loZsRzvyCnWn7mWPF7jnV21MRKj/Kack4=\n-----END -----\n'
+                'mspid': 'OrdererMSP',
+              },
+              'nonce': '2f7a09633b3ad407c126e87c02cefeec50850406c7baf7ec'
+            },
+          },
+        ],
+        'value': {
+          'index': 0,
+        },
+      },
+      [0],
+    ],
+  },
+}
